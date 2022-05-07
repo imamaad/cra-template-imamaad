086fbe247ffb21c7fb5f66feed5779e324f21ad9
mohammad hossine rezazadeh <mrezazadeh78.mr@gmail.com>
2022-05-07 12:58:34 +0430

    description

--- template/src/routes/ManageRoutes.tsx
+++ template/src/routes/ManageRoutes.tsx
@@ -6,6 +6,7 @@ export const ManageRoutes = () => {
     return (
         <Routes>
             <Route index element={<DashboardPage/>}/>
+            <Route index element={<AboutMePage/>}/>
         </Routes>
     );
 }
